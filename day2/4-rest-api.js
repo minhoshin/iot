342fe8129f66bd6c68f5818959ef4d8ec849e284
minhoshin <minho.shin@gamil.com>
2018-09-16 18:05:34 +0000

    day2

--- day2/4-rest-api.js
+++ day2/4-rest-api.js
@@ -1,17 +1,20 @@
 const express = require('express');
 const app = express();
+const cors = require('cors');
+
+app.use(cors());
 
 app.get('/', (req, res) => {
 	res.send('Hello World!');
 });
 
 app.get('/ledon', (req, res) => {
-	console.log('led on');
+	console.log('led on ' + Date());
 	res.send('led on');
 });
 
 app.get('/ledoff', (req, res) => {
-	console.log('led off');
+	console.log('led off ' + Date());
 	res.send('led off');
 });
 
